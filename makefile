3eff3c5d131353318062e8899a2ced0a7416bd4d
SantiagoGarciaVazquez <159966508+SantiagoGarciaVazquez@users.noreply.github.com>
2024-04-16 23:27:32 +0000

    Version 2

--- makefile
+++ makefile
@@ -1,10 +1,5 @@
-OBJETIVO : INGREDIENTES
-	PASO 1
-	PASO 2
-	PASO 3
+bin/test : src/tui_test.cpp
+	g++ src/tui_test.cpp -o bin/test -std=c++2a -lftxui-screen -lftxui-component -lftxui-dom         
 
-bin/test : src/tui:test.cpp
-		g++ src/main.cpp -o bin/test
-
-ejecutar : bin/space
+ejecutar : bin/test
 	./bin/test
--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -1,25 +1,23 @@
-#include <ftxui/include/ftxui/dom/node.hpp>
+#include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/screen/string.hpp>
-using namespace;
 
-int main(int argc, char const *argv[])
-{
-    //crea los elementos de la interdaz del usuario
-    Element label = text("Hola, Mundo");
-    Element container  = hbox({label});
+using namespace ftxui;
 
-    //Crea la pantalla y establece el contenimdo. 
-    auto screen  = Screen::Create{
-        Dimension::Full(),
+int main() {
+
+    // Crea los elementos de la interfaz de usuario.
+    Element label = text("Hola, mundo!");
+    Element container = hbox({label});
+
+    // Crea la pantalla y establece el contenido.
+    auto screen = Screen::Create(
+        Dimension::Full(), 
         Dimension::Fit(container)
-    }
+    );
 
-    Render(screen, container);
+    Render(screen,container);
     screen.Print();
 
-
-
-    
     return 0;
 }
