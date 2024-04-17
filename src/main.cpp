508a304849b04d9430588790a7488e4df28d7271
SantiagoGarciaVazquez <159966508+SantiagoGarciaVazquez@users.noreply.github.com>
2024-04-17 00:22:11 +0000

    Version 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <string>
+#include<thread>
+using namespace std;
+using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    std:cout<<"Space Invaders">>;
-    return 0;
+    Element lienzo = hbox({
+        spinner(21,3) | bold
+    });
+
+    Screen pantalla = Screen::Create(
+        Dimension::Full(),
+        Dimension::Fit(lienzo)
+    );
+
+    Render(pantalla,lienzo);
+    pantalla.Print();
+    pantalla.ResetPosition();
+    this_thread::sleep_for(0.1s);
 }
