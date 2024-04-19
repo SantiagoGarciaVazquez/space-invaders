80f328fb499659ac5ea1266be1995cee63ad8d40
SantiagoGarciaVazquez <159966508+SantiagoGarciaVazquez@users.noreply.github.com>
2024-04-19 23:30:10 +0000

    Version 4

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,34 @@ using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
+    fstream archivo;
+
+    archivo.open("/assets/images/cañon.txt");
+    string cañon;
+    archivo >> cañon;
+    archivo.close();
+
+    archivo.open("/assets/images/alien.txt");
+    string alien;
+    archivo >> alien;
+    archivo.close();
+
+
+    int fotograma = 0;
+    while(true){
+        fotograma++;
+
+        Element personaje = 
+
+        Screen pantalla = Screen::Create{
+            Dimension::Full();
+        }
+    }
+
+
+
+
+
     Element lienzo = hbox({
         spinner(21,3) | bold
     });
